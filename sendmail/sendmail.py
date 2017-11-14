6f93705000af4bd995d2484f0844df1c4dd70e1b
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2017-11-14 19:43:23 +0530

    Edited SendMail fnction to send image as attachment

--- sendmail/sendmail.py
+++ sendmail/sendmail.py
@@ -1,9 +1,12 @@
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 
-#sender, [recipientsList], htmlmsg, subject, serverIP
+#sender, [recipientsList], htmlmsg, subject, serverIP, attachment
 def sendmail(**kwargs):
 	try:
+		HasImg=False
 		sender=recipientlist=htmlmsg=subject=serverIP=None
 		if kwargs is not None:
 			for k,v in kwargs.iteritems():
@@ -12,22 +15,43 @@ def sendmail(**kwargs):
 				if k=='htmlmsg' : htmlmsg=kwargs[k]
 				if k=='subject' : subject=kwargs[k]
 				if k=='serverIP' : serverIP=kwargs[k]
-		body = htmlmsg
-		msg = MIMEText(body, 'html')
-		msg['Subject'] = subject
-		msg['From'] = sender
-		msg['To'] = ", ".join(recipientlist)
+				if k=='attachment':
+					attachment=kwargs[k]
+					HasImg=True
 		
-		#sending
-		session = smtplib.SMTP(serverIP)
-		session.ehlo()
-		session.starttls()
-		session.ehlo
-		print 'Sending to recipients:-'
-		print recipientlist
-		send_it = session.sendmail(sender, recipientlist, msg.as_string())
-		session.quit()
-	except e:
+			if HasImg == True:
+				msg = MIMEMultipart()
+				msg['To'] = ", ".join(recipientlist)
+				msg["From"] = sender
+				msg["Subject"] = subject
+
+				msgText = MIMEText('<b>%s</b><br><img src="cid:%s"><br>' % (htmlmsg, attachment), 'html')  
+				msg.attach(msgText)   # Added, and edited the previous line
+
+				fp = open(attachment, 'rb')                                                    
+				img = MIMEImage(fp.read())
+				fp.close()
+				img.add_header('Content-ID', '<{}>'.format(attachment))
+				msg.attach(img)
+			
+			else:
+				body = htmlmsg
+				msg = MIMEText(body, 'html')
+				msg['Subject'] = subject
+				msg['From'] = sender
+				msg['To'] = ", ".join(recipientlist)
+				
+			#sending
+			session = smtplib.SMTP(serverIP)
+			session.ehlo()
+			session.starttls()
+			session.ehlo
+			print 'Sending to recipients:-'
+			print recipientlist
+			send_it = session.sendmail(sender, recipientlist, msg.as_string())
+			session.quit()
+			
+	except Exception, e:
 		print 'Exception {0}'.format(str(e))
 		raw_input()
 		exit()
--- test.py
+++ test.py
@@ -2,7 +2,12 @@ from sendmail import sendmail
 from listfunctions import listfunctions
 from directoryfunctions import directoryfunctions
 
-#sendmail.sendmail(sender='pjayaraj@amd.com',recipientlist=['pranoy.jayaraj@amd.com'],htmlmsg='TestMessage', subject='Test',	serverIP='10.180.168.6')
+attachment=r'C:\Users\pjayaraj\Documents\GitHub\pjayaraj_repository\image.png'
+sendmail.sendmail(sender='pjayaraj@amd.com',
+				recipientlist=['pranoy.jayaraj@amd.com'],
+				htmlmsg='TestMessage',
+				subject='Test',
+				serverIP='10.180.168.6')
 #print listfunctions.GetMaxMin([1,2,3,4,5])
 #print listfunctions.SortList(ord='dec',list=[0,0,-1,1,2,3])
 #print listfunctions.CreateHTMLTable(list=['Pranoy','Jayaraj','28'], header=['First','Last','Age'])
