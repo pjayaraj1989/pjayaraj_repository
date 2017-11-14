f874f4e2aba8e8306898455eb5397d025caac631
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2017-11-14 23:01:49 +0530

    Minor tweaks

--- sendmail/sendmail.py
+++ sendmail/sendmail.py
@@ -3,7 +3,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 
-#sender, [recipientsList], htmlmsg, subject, serverIP, attachment
+#sender, [recipientsList], htmlmsg, subject, serverIP, attachment 
 def sendmail(**kwargs):
 	try:
 		HasImg=False
