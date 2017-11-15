2a61d6f8ca6dc53be350791cee2fda450f808dca
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2017-11-15 12:53:56 +0530

    Minor tweaks

--- test.py
+++ test.py
@@ -3,14 +3,10 @@ from listfunctions import listfunctions
 from directoryfunctions import directoryfunctions
 from generatepasscode import generatepasscode
 from processfunctions import processfunctions
-'''
-attachment=r'C:\Users\pjayaraj\Documents\GitHub\pjayaraj_repository\image.png'
-sendmail.sendmail(sender='pjayaraj@amd.com',
-				recipientlist=['pranoy.jayaraj@amd.com'],
-				htmlmsg='TestMessage',
-				subject='Test',
-				serverIP='10.180.168.6')
-'''
+
+#attachment=r'C:\Users\pjayaraj\Documents\GitHub\pjayaraj_repository\image.png'
+#sendmail.sendmail(sender='pjayaraj@amd.com',recipientlist=['pranoy.jayaraj@amd.com'],
+#				htmlmsg='TestMessage',subject='Test',serverIP='10.180.168.6')
 #print listfunctions.GetMaxMin([1,2,3,4,5])
 #print listfunctions.SortList(ord='dec',list=[0,0,-1,1,2,3])
 #print listfunctions.CreateHTMLTable(list=['Pranoy','Jayaraj','28'], header=['First','Last','Age'])
